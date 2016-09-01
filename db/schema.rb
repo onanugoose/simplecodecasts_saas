3c76e37da0325dad9df6e844afa993bb37d395e4
Fred Warsaw <fred.warsaw@yahoo.com>
2016-09-01 15:39:58 +0000

    Created plan file

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160901080340) do
+ActiveRecord::Schema.define(version: 20160901102446) do
 
   create_table "contacts", force: true do |t|
     t.string   "name"
@@ -19,6 +19,9 @@ ActiveRecord::Schema.define(version: 20160901080340) do
     t.text     "comments"
     t.datetime "created_at"
     t.datetime "updated_at"
+  end
+
+  create_table "plans", force: true do |t|
   end
 
   create_table "users", force: true do |t|
